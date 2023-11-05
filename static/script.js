426d1446e63b2749d5250ecb5cb892f4ae95900f
Kshitij Kulshrestha <91348714+K-Kulshrestha@users.noreply.github.com>
2023-11-05 05:40:50 -0600

    added ui to navigate thru the app

--- static/script.js
+++ static/script.js
@@ -1,12 +1,33 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const analyzeButton = document.getElementById("location-button");
-    const settingsButton = document.getElementById("bType-button");
+function toggleMenu() {
+    var isSidebarOpen = sidebar.style.left === "0px" || sidebar.style.left === "";
+    
+    // Toggle the sidebar and change its visibility
+    if (isSidebarOpen) {
+        sidebar.style.left = "-250px";
+        sidebar.style.visibility = "hidden";
+    } else {
+        sidebar.style.left = "0px";
+        sidebar.style.visibility = "visible";
+    }
+    
+    // Toggle the 'sidebar-open' class on the menuToggle element
+    menuToggle.classList.toggle("sidebar-open", !isSidebarOpen);
+    
+    if (isSidebarOpen) {
+        // Sidebar is open, hide the hamburger icon
+        menuToggle.style.display = "none";
+    } else {
+        // Sidebar is closed, show the hamburger icon
+        menuToggle.style.display = "block";
+    }
+    
+    // Update the sidebar's transparency and backdrop filter based on its open/closed state
+    if (isSidebarOpen) {
+        sidebar.style.backgroundColor = "rgba(51, 51, 51, 0.8)";
+        sidebar.style.backdropFilter = "blur(10px)";
+    } else {
+        sidebar.style.backgroundColor = "transparent";
+        sidebar.style.backdropFilter = "none";
+    }
+}
 
-    analyzeButton.addEventListener("click", function () {
-        alert("Searching by Location...");
-    });
-
-    settingsButton.addEventListener("click", function () {
-        alert("Searching by Business Information...");
-    });
-});
